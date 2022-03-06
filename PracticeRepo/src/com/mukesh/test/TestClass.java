7f36ffac4aa2b3a7d3077049a45fc2ebd75a7384
mukesh <ishimukesh8@gmail.com>
2022-03-06 23:26:11 +0530

    second modification by TL

---
+++ PracticeRepo/src/com/mukesh/test/TestClass.java
@@ -0,0 +1,14 @@
+package com.mukesh.test;
+
+import com.mukesh.beans.Utility;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		Utility ut=new Utility();
+		String res=ut.m1();
+		System.out.println(res);
+
+	}
+
+}
